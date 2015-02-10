84e0705d81137538b7174cff8f70988904c95f50
Christopher Hartley <chartley@solutionreach.com>
2015-02-09 21:35:35 -0700

    Added some comments

--- Warehouse.h
+++ Warehouse.h
@@ -17,15 +17,15 @@ class Warehouse
 {
 public:
     Warehouse(); // default constructor
-    Warehouse(std::string _location);
+    Warehouse(std::string _location); // constructor with string location
     Warehouse(const Warehouse & other); // copy constructor
 
-    std::string get_location() const;
-    int get_inventory_size() const;
-    void receive_food_item(std::string _upc, int n, int shelf_life);
-    void remove_food_item(std::string _upc, int n);
-    void remove_expired();
-    int item_count(std::string upc) ;
+    std::string get_location() const; // returns the string location name
+    int get_inventory_size() const; // returns the int size of inventory
+    void receive_food_item(std::string _upc, int n, int shelf_life); // adds food item, params - string upc, int number of items, int shelf life
+    void remove_food_item(std::string _upc, int n); // removes food item, params - string upc, int number of items to remove
+    void remove_expired(); // removes all old items
+    int item_count(std::string upc) ; //returns the count, params - sting upc, retuns - int amount
 
     bool operator<(const Warehouse& rhs) const;
     bool operator==(const Warehouse& rhs) const;
